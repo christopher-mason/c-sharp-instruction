76ce9cb2cd5728be5101dbf2b435e5c2530d5c1a
Chris Mason <m1achris43@gmail.com>
2020-11-30 21:06:56 -0500

    init commit

---
+++ proj-02-01-student-registration/proj-02-01-student-registration/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace proj_02_01_student_registration {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Student Registration Form");
+
+            Console.Write("Enter First Name: ");
+            String firstName = Console.ReadLine();
+            Console.Write("Enter Last Name: ");
+            String lastName = Console.ReadLine();
+            Console.Write("Enter year of birth: ");
+            int year = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            String tempPWD = firstName + "*" + year;
+
+            Console.WriteLine("Welcome " + firstName + " " + lastName + "!");
+            Console.WriteLine("Your registration is complete!");
+            Console.WriteLine("Your temporary password is: " + tempPWD);
+
+            }
+        }
+    }
