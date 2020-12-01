1917e556073182d5189dcbd81dedf3dcee5bc775
Chris Mason <m1achris43@gmail.com>
2020-11-30 21:44:42 -0500

    More Practice

---
+++ ch03_primitives/ch03_primitives/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ch03_primitives {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Primitive Types");
+
+            int n1 = 5;
+            double d1 = 5.5;
+            bool success = false;
+            Int32 n2 = 7;
+            Double d2 = 11.12;
+            Console.WriteLine("int sum = " + (n1 + n2));
+
+            int n = 0;
+            n++;
+            Console.WriteLine(n);
+
+            }
+        }
+    }
---
+++ proj-02-02-Grade-Converter/proj-02-02-Grade-Converter/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace proj_02_02_Grade_Converter {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Letter Grade Converter");
+            }
+        }
+    }
