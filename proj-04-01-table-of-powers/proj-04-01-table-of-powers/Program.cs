d6c6c743f441ac3f94a6d24231b85aaafdfe2279
Chris Mason <m1achris43@gmail.com>
2020-12-02 18:59:12 -0500

    Chapter 4 Practice

---
+++ proj-04-01-table-of-powers/proj-04-01-table-of-powers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace proj_04_01_table_of_powers {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome tot he Squares and Cubes Table");
+
+            int i;
+            String choice = "y";
+            while (choice.Equals("y")) {
+                Console.WriteLine("Enter Integer: ");
+                i = Int32.Parse(Console.ReadLine());
+                int squared = 0;
+                int cubed = 0;
+
+                Console.WriteLine("Number\tSquared\tCubed" + "\n" +
+                       "======\t=======\t=======");
+                for (int p = 1; p <= i; p++) {
+                    squared = (int)Math.Pow(p, 2);
+                    cubed = (int)Math.Pow(p, 3);
+                    Console.WriteLine(p + "\t" + squared + "\t" + cubed);
+                    }
+
+                Console.Write("Continue? (y/n)");
+                choice = Console.ReadLine();
+
+                }
+            Console.WriteLine("Bye!");
+            }
+        }
+    }
---
+++ proj-04-02-factorial-calculator/proj-04-02-factorial-calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace proj_04_02_factorial_calculator {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Factorial Calculator");
+
+            int number = 0;
+            long factorial = 1;
+
+            String choice = "y";
+            while (choice.Equals("y")) {
+                Console.Write("Enter an integer that is greater than zero but less than 10: ");
+                number = Int32.Parse(Console.ReadLine());
+                factorial = 1;
+                for (int i = 1; i <= number; i++) {
+                    factorial = factorial * i;
+                    }
+                if (number > 10) {
+                    Console.WriteLine("Error! Invalid integer.");
+                    }
+                Console.WriteLine("The factorial of " + number + " is " + factorial);
+                Console.WriteLine("Continue? (y/n)");
+                choice = Console.ReadLine();
+                }
+
+
+            Console.WriteLine("Bye!");
+
+            }
+        }
+    }
