b0a93f4aa8d7a0d95f0949b0560d4bd5cb1a0be4
Chris Mason <m1achris43@gmail.com>
2020-12-02 18:26:37 -0500

    Practice

---
+++ ch03-for-loop/ch03-for-loop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ch03_for_loop {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Creating for-loops");
+            // indexed for loop that will print hello n times
+            // n will be entered by user
+
+            Console.Write("Enter a whole number: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine("Hello! " + i);
+                }
+
+
+            }
+        }
+    }
---
+++ proj-02-2-grade-converter/proj-02-2-grade-converter/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace proj_02_2_grade_converter {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Letter Grade Converter");
+            }
+        }
+    }
