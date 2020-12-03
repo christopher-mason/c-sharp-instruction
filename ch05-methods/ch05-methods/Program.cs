9cb381672a28422fdf29bae7ec1d8cb3f48f60ed
Chris Mason <m1achris43@gmail.com>
2020-12-02 21:05:38 -0500

    More Chapter 5 Practice

---
+++ ch05-methods/ch05-methods/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ch05_methods {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Call some methods");
+            // prompts user to enter whole number
+            // convert that input to a number
+            // print the number
+
+            int n = GetInt("Enter a whole number: ");
+            Console.WriteLine("You entered the number: " + n);
+
+            int age = GetInt("Enter actor age: ");
+            Console.WriteLine("Actor age is: " + age);
+
+            Console.WriteLine("Bye!");
+            }
+
+        private static int GetInt(String prompt) {
+            int n = 0;
+            Boolean success = false;
+            while (!success) {
+                Console.Write(prompt);
+                try {
+                    n = int.Parse(Console.ReadLine());
+                    success = true;
+                }
+                catch (Exception e) {
+                    Console.WriteLine("Invalid entry. Not a whole number. Try again.");
+                }
+            }
+            return n;
+        }
+    }
+}
---
+++ proj-05-01-dice-roller/proj-05-01-dice-roller/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace proj_05_01_dice_roller {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Dice Roller");
+
+            int dice1 = GetDiceRoll();
+            int dice2 = GetDiceRoll();
+
+            String message = "Dice 1: " + dice1 + "\n" + "Dice 2: " + dice2 + "\n" + "Total: " + (dice1 + dice2);
+            Console.WriteLine(message);
+            PrintExtraMessage(dice1, dice2);
+
+            }
+
+        static int GetDiceRoll() {
+            Random rnd = new Random();
+            int diceRoll = rnd.Next(1, 7);
+            return diceRoll;
+
+            }
+        static void PrintExtraMessage(int dice1, int dice2) {
+            if (dice1 + dice2 == 2) {
+                Console.WriteLine("Snake Eyes");
+                } else if (dice1 + dice2 == 12) {
+                Console.WriteLine("Box cars!");
+                }
+            }
+        }
+    }
