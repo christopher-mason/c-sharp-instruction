e2e1c62a553d98a3c6a47f95726ad5700f959f84
Chris Mason <m1achris43@gmail.com>
2020-12-05 11:55:02 -0500

    Array Practice

--- ch07-product-manager/ch07-product-manager/ch07_product_manager/MyConsole/Console.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace ch07_product_manager.MyConsole {
-    internal class Console {
-        }
-    }
---
+++ proj-12-02-wizard-inventory/proj-12-02-wizard-inventory/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using proj_12_02_wizard_inventory.ui;
+
+namespace proj_12_02_wizard_inventory {
+    class Program {
+        //static final int MAX_INVENTORY_SIZE = 4;
+
+        static void Main(string[] args) {
+            Console.WriteLine("The Wizard Inventory game\n");
+            DisplayMenu();
+
+            List<String> inventory = new ArrayList<>();
+            inventory.Add("wooden staff");
+            inventory.Add("wizard hat");
+            inventory.Add("cloth choes");
+
+            String command = "go";
+
+            while (!command.Equals("exit")) {
+                command = MyConsole.getString("Command: ");
+
+                switch (command) {
+                    case "show":
+
+                    default:
+                        break;
+                    }
+                }
+
+
+            }
+
+        private static void DisplayMenu() {
+            Console.WriteLine("COMMAND MENU");
+            Console.WriteLine("show - Show all items");
+            Console.WriteLine("grab - Grab an item");
+            Console.WriteLine("edit - Edit an item");
+            Console.WriteLine("drop - Drop an item");
+            Console.WriteLine("exit - Exit program");
+            }
+
+        private static void ShowInventory(List<string> inventory) {
+            int oneBasedIndex = 1;
+
+            for (String item : inventory) {
+                Console.WriteLine(oneBasedIndex + "." + item);
+                oneBasedIndex++;
+                }
+            }
+
+        }
+    }
