c7e9f9dfc6189fb80e87d5f74dc1c58091d11d7c
Chris Mason <m1achris43@gmail.com>
2020-12-05 10:11:45 -0500

    Chapter 7

---
+++ ch07-product-manager/ch07-product-manager/Program.cs
@@ -0,0 +1,25 @@
+﻿using ch07_product_manager.business;
+using System;
+
+namespace ch07_product_manager {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Product Manager");
+
+            Product p1 = new Product();
+            p1.code = ".net";
+            p1.description = "Murach's C# and .NET";
+            p1.price = 58.99;
+            Console.WriteLine("p1= " + p1);
+
+            Product p2 = new Product("java", "Murach's Java Programming", 59.50);
+            Console.WriteLine($"p2 = {p2}");
+
+            
+            
+
+            Console.WriteLine("Bye!");
+
+            }
+        }
+    }
---
+++ ch07-product-manager/ch07-product-manager/ch07_product_manager/MyConsole/Console.cs
@@ -0,0 +1,4 @@
+﻿namespace ch07_product_manager.MyConsole {
+    internal class Console {
+        }
+    }
